02081b767f729f7f5c14d8aec96359e660f4e82c
praveenkesava <139122233+praveenkesava@users.noreply.github.com>
2023-07-10 15:30:32 +0530

    Update Lab experiment 1.py

--- Lab experiment 1.py
+++ Lab experiment 1.py
@@ -1,13 +1,13 @@
-num = int(input("Enter an integer: "))
-if num % 2 != 0:
+n = int(input("Enter an integer: "))
+if n% 2 != 0:
     factorial = 1
-    for i in range(2, num + 1):
+    for i in range(2, n + 1):
         factorial *= i
         num_digits = len(str(factorial))
     print("Factorial:", factorial)
     print("Number of digits in factorial:", num_digits)
 else:
-    num_str = str(num)
+    num_str = str(n)
     if num_str == num_str[::-1]:
         print("The number is a palindrome.")
     else:
