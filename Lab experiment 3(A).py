13b0f640a61306eeb9dc6c4b58f40e5546646b53
praveenkesava <139122233+praveenkesava@users.noreply.github.com>
2023-07-10 15:33:08 +0530

    Update Lab experiment 3(A).py

--- Lab experiment 3(A).py
+++ Lab experiment 3(A).py
@@ -1,19 +1,19 @@
 def find_occurrence(numbers, target):
-    positive_indices = []
-    negative_indices = []
+    positive = []
+    negative = []
     count = 0
 
     for i in range(len(numbers)):
         if numbers[i] == target:
-            positive_indices.append(i)
-            negative_indices.append(-len(numbers) + i)
+            positive.append(i)
+            negative.append(-len(numbers) + i)
             count += 1
 
-    return count, positive_indices, negative_indices
+    return count, positive, negative
 
 numbers = list(map(int, input("Enter the list of numbers :")))
 target = int(input("Enter the element to be found: "))
-occurrence, positive_indices, negative_indices = find_occurrence(numbers, target)
+occurrence, positive, negative = find_occurrence(numbers, target)
 print("Element", target, "occurs", occurrence, "time(s) in the list.")
-print("Positive Index:", ", ".join(map(str, positive_indices)))
-print("Negative Index:", ", ".join(map(str, negative_indices)))
+print("Positive Index:", ", ".join(map(str, positive)))
+print("Negative Index:", ", ".join(map(str, negative)))
